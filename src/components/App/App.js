0894e93fd56ab4345d6deb45d99010d60fe6394b
shanGan21 <shanthikg@gmail.com>
2023-12-04 22:15:24 -0500

    update3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,10 +24,13 @@ const business = {
     
 
 const App = () => {
+  const searchYelp = (term, location, sortBy) => {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  };
   return (
     <div className={styles.App}>
       <h1>ravenous</h1>
-      <SearchBar />  
+      <SearchBar searchYelp={searchYelp} />  
       <BusinessList businesses={businesses} />
     </div>
   );
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./SearchBar.module.css";
 
 
@@ -8,30 +8,68 @@ const sortByOptions = {
     "Most Reviewed": "review_count",
   };
 
-function SearchBar(){
+function SearchBar({searchYelp}){
 
-    const renderSortByOptions = () => {
-        return Object.keys(sortByOptions).map((sortByOption) => {
-          let sortByOptionValue = sortByOptions[sortByOption];
-          return <li key={sortByOptionValue}>{sortByOption}</li>;
-        });
-      };
+  const [term, setSearchTerm] = useState("");
+  const [location, setLocation] = useState("");
+  const [sortBy, setSortBy] = useState("best_match");
 
+  const getSortByClass = (sortByOption) => {
+    if (sortBy === sortByOption) {
+      return styles.active;
+    }
+    return "";
+  };
+
+  const handleSortByChange = (sortByOption) => {
+    setSortBy(sortByOption);
+  };
+  const handleTermChange=(event)=>{
+    setSearchTerm(event.target.value);
+  }
+
+  const handleLocationChange=(event)=>{
+    setLocation(event.target.value);
+  }
+  
+  const handleSearch = (event) => {
+    event.preventDefault();
+    searchYelp(term, location, sortBy);
+  };
+
+  const renderSortByOptions = () => {
+    return Object.keys(sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return (
+        <li
+          className={getSortByClass(sortByOptionValue)}
+          key={sortByOptionValue}
+          onClick={() => {
+            handleSortByChange(sortByOptionValue);
+          }}
+        >
+          {sortByOption}
+        </li>
+      );
+    });
+  };
     
 
       return (
         <div className={styles.SearchBar}>
-          <div className={styles.SearchBarSortOptions}>
-            <ul>{renderSortByOptions()}</ul>
-          </div>
-          <div className={styles.SearchBarFields}>
-            <input placeholder="Search Businesses" />
-            <input placeholder="Where?" />
-          </div>
-          <div className={styles.SearchBarSubmit}>
-            <a>Let's Go</a>
-          </div>
+      <div className={styles.SearchBarSortOptions}>
+        <ul>{renderSortByOptions()}</ul>
+      </div>
+      <form onSubmit={handleSearch}>
+        <div className={styles.SearchBarFields}>
+          <input placeholder="Search Businesses" onChange={handleTermChange} />
+          <input placeholder="Where?" onChange={handleLocationChange} />
         </div>
+        <div className={styles.SearchBarSubmit}>
+          <button type="submit">Let's Go</button>
+        </div>
+      </form>
+    </div>
       );
 }
 
